bf9f0b15f020dc95a3a3c736831ff14912e5a37f
Semanya <semanya27@gmail.com>
2021-10-26 03:04:33 +0300

    111

---
+++ moidule02/second/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+type (
+	AmericanVelocity float64
+	EuropeanVelocity float64
+)
+
+var (
+	first_metr  = 120.4
+	second_metr = 130.0
+	American    AmericanVelocity
+	European    EuropeanVelocity
+)
+
+func main() {
+	first_kilometr := EuropeanVelocity((first_metr * 18) / 5)
+	fmt.Print(first_kilometr)
+	fmt.Printf(" and has type:  %T\n", first_kilometr)
+	second_milya := AmericanVelocity((second_metr * 18) / (5 * 1.609))
+	fmt.Print(second_milya)
+	fmt.Printf(" and has type:  %T\n", second_milya)
+}
