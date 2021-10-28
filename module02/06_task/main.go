97c05a97683f6ed8ac648ed38ad36b935879fbaf
Semanya <semanya27@gmail.com>
2021-10-28 20:32:46 +0300

    111

--- module02/06_task/main.go
+++ module02/06_task/main.go
@@ -4,25 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
+)
+
+var (
+	pathin       string = "/Users/u19502010/golndbase/golandbase/module02/06_task/06_task_in.txt"
+	pathout      string = "/Users/u19502010/golndbase/golandbase/module02/06_task/06_task_out.txt"
+	i, b         int
+	resultString string
 )
 
 func main() {
-	pathin := "/Users/u19502010/golndbase/golandbase/module02/06_task/06_task_in.txt"
-	pathout := "/Users/u19502010/golndbase/golandbase/module02/06_task/06_task_out.txt"
 	CheckFileExist(pathout)
-	getFile(pathin)
+	logTime()
 }
 
-func getFile(path string) {
-	f, _ := os.Open(path)
-	defer f.Close()
+func logTime() {
+	defer duration(track("Duration"))
+	writeOnFile(pathin, pathout)
+}
 
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	fmt.Printf("%v: %v\n", msg, time.Since(start))
+}
+func writeOnFile(pathin string, pathout string) {
+	f, _ := os.Open(pathin)
+	var lines []string
 	s := bufio.NewScanner(f)
-	i := 0
 	for s.Scan() {
-		i++
-		fmt.Println(i, s.Text())
+		lines = append(lines, s.Text())
 	}
+	fo, err := os.OpenFile(pathout, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w := bufio.NewWriter(fo)
+	for _, row := range lines {
+		i++
+		resultString = fmt.Sprint(i) + " " + row + "\n"
+		nn, _ := w.WriteString(resultString)
+		b = b + nn
+	}
+	defer fo.Close()
+	defer fmt.Println("Количество байт", b, "Количество строк", i)
+	defer f.Close()
+	defer w.Flush()
 }
 
 func CheckFileExist(fileName string) bool {
