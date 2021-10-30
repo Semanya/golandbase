820576ba747a929bf8b2eb3457ac3c4225adbf82
Semanya <semanya27@gmail.com>
2021-10-30 18:15:55 +0300

    Вот тебе коммит Вова - не души

---
+++ module02/08_task/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	workPath = "/Users/u19502010/golndbase/golandbase"
+)
+
+var (
+	pathin = workPath + "/module02/08_task/08_task_in.txt"
+	result string
+	i      = 0
+)
+
+func main() {
+	readFile(pathin)
+}
+
+func readFile(pathin string) {
+	f, err := os.Open(pathin)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	s := bufio.NewReader(f)
+	for {
+		_, err := s.ReadString('\n')
+		i++
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+	fmt.Printf("Total strings: %d", i)
+}
