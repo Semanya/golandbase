bd05f87dca8034d94cd487018acca80cccf8be22
Semanya <semanya27@gmail.com>
2021-11-01 23:00:32 +0300

    new

---
+++ module03/02_task/gopackages/go.mod
@@ -0,0 +1,5 @@
+module gopackages
+
+go 1.17
+
+require github.com/huandu/xstrings v1.2.1
---
+++ module03/02_task/gopackages/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	newcolor "gopackages/color"
+	. "gopackages/wordz" //Добавляем пакет wordz через точку
+
+	"github.com/huandu/xstrings"
+)
+
+func main() {
+	newcolor.Greet()
+	fmt.Println("Hello world")
+
+	fmt.Println(Hello)    //Вызов переменной из пакета wordz
+	fmt.Println(Random()) //Вызов функции из пакета wordz
+	fmt.Println(xstrings.Shuffle("Moscow"))
+
+}
