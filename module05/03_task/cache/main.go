08a0fa348d016f3eb7d4e1303316d47c1ceacf23
Semanya <semanya27@gmail.com>
2021-11-16 18:02:24 +0300

    add 03 task

--- module05/03_task/cache/main.go
+++ module05/03_task/cache/main.go
@@ -1,16 +1,25 @@
 package main
 
-import "time"
+import (
+	"time"
+	"sync"
+	"fmt"
+)
 
 type Cache struct {
 	storage map[string]int
+	mu         sync.Mutex
 }
 
 func (c *Cache) Increase(key string, value int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.storage[key] += value
 }
 
 func (c *Cache) Set(key string, value int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.storage[key] = value
 }
 
@@ -20,4 +29,24 @@ func (c *Cache) Get(key string) int {
 
 func (c *Cache) Remove(key string) {
 	delete(c.storage, key)
+}
+
+func main() {
+	// months := map[string]int{
+	// 	"Jan": 31,
+	// 	"Feb": 28,
+	// 	"Mar": 31,
+	//   }
+	months := &Cache{
+		storage: map[string]int{
+			"Jan": 31,
+			"Feb": 28,
+			"Mar": 31,
+		  },
+	}
+    go months.Increase("May", 7)
+	months.Increase("Apr", 7)
+	time.Sleep(time.Second)
+	fmt.Println(months.Get("Apr"))
+	fmt.Println(months.Get("May"))
 }
