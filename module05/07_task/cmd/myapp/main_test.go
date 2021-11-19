348d1fba2299eb75325b0a6b6932a151762e0d21
Semanya <semanya27@gmail.com>
2021-11-19 17:31:35 +0300

    add 07 task

---
+++ module05/07_task/cmd/myapp/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	// "fmt"
+	"sync"
+	"testing"
+)
+
+type Counter struct {
+	A int
+	B int
+}
+
+var pool = sync.Pool{
+	New: func() interface{} { return new(Counter) },
+}
+
+func PlusOne(s *Counter) {
+	s.A++
+	s.B++
+}
+
+func BenchmarkWithoutPool(b *testing.B) {
+	var s *Counter
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 10000; j++ {
+			// b.StopTimer()
+			s = &Counter{A: 1, B: 2}
+			PlusOne(s)
+			// b.StartTimer()
+		}
+	}
+}
+
+func BenchmarkWithPool(b *testing.B) {
+	var p *Counter
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 100; j++ {
+			p = pool.Get().(*Counter)
+			p.A = 1
+			p.B = 2
+			// b.StopTimer()
+			PlusOne(p)
+			// b.StartTimer()
+			pool.Put(p)
+		}
+	}
+}
