14e851fe433c6786bb461b94087c8ef92bb38d02
Semanya <semanya27@gmail.com>
2021-11-24 00:47:52 +0300

    add 04 task

--- module06/04_task/internal/pkg/util/util_test.go
+++ module06/04_task/internal/pkg/util/util_test.go
@@ -1,26 +1,42 @@
 package util
 
 import (
+	"errors"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+type testpairreversint struct {
+	values interface{}
+	result int
+	err    error
+}
+
+var testsreversint = map[string]testpairreversint{
+	"4 case": {0.123, 0, errors.New("not int")},
+	"1 case": {2147483648, 0, nil},
+	"2 case": {-649, -946, nil},
+	"3 case": {0, 0, nil},
+}
 
 type testpair struct {
 	check   []int
 	currect bool
 }
 
-var tests = []testpair{
-	{[]int{123, 321, 331231, 2321}, false},
-	{[]int{-649, -946, -233121, -3213}, false},
-	{[]int{-64, 46, 3121, 213}, false},
+var tests = map[string]testpair{
+	"1 case": {[]int{123, 321, 331231, 2321}, false},
+	"2 case": {[]int{-649, -946, -233121, -3213}, false},
+	"3 case": {[]int{46, 46, 3121, 213}, true},
 }
 
 func TestContainsDuplicate(t *testing.T) {
 	req := require.New(t)
-	for _, pair := range tests {
-		v := ContainsDuplicate(pair.check)
-		req.Equal(v, pair.currect)
+	for name, pair := range tests {
+		t.Run(name, func(t *testing.T) {
+			v := ContainsDuplicate(pair.check)
+			req.Equal(v, pair.currect)
+		})
 	}
 
 }
@@ -34,6 +50,18 @@ func TestIsPalindrome(t *testing.T) {
 		}
 	}
 	t.Run("simple case", PalCase(355, false))
-	t.Run("zero values", PalCase(533, false))
+	t.Run("zero values", PalCase(-533, false))
 	t.Run("negative values", PalCase(725, false))
+}
+
+func TestReverseInt(t *testing.T) {
+	req := require.New(t)
+	for name, pair := range testsreversint {
+		t.Run(name, func(t *testing.T) {
+			v, err := ReverseInt(pair.values)
+			req.Equal(err, pair.err)
+			req.Equal(v, pair.result)
+		})
+	}
+
 }
